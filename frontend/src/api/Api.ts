1f2064faaea4b1a23010fd5f48f2775d6bbf84d2
santosh <santehero8@gmail.com>
2023-11-23 00:18:39 +0545

    replaced backend url

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL:"https://resourcify-pl7s.onrender.com/api",
     withCredentials:true
 
 })
